9982837cb048f77ac952634912dd8a965ecfd396
Guilherme Faraco-laptop <guirfv29@gmail.com>
2025-07-15 19:51:22 -0300

    Update: Transferindo as responsabilidades de conversão que estavam nas dimensoes dentro da camada gold para a camada silver

--- postgres_dw/models/gold/dim_risk_factors.sql
+++ postgres_dw/models/gold/dim_risk_factors.sql
@@ -1,20 +1,20 @@
 -- Cria uma dimensão para cada combinação única de fatores de risco (comorbidades).
 WITH risk_factors AS (
     SELECT DISTINCT
-        ("PUERPERA" = '1') AS is_puerpera,
-        ("CARDIOPATI" = '1') AS has_chronic_cardiovascular_disease, 
-        ("HEMATOLOGI" = '1') AS has_chronic_hematologic_disease, 
-        ("SIND_DOWN" = '1') AS has_down_syndrome,
-        ("HEPATICA" = '1') AS has_chronic_liver_disease,
-        ("ASMA" = '1') AS has_asthma,
-        ("DIABETES" = '1') AS has_diabetes,
-        ("NEUROLOGIC" = '1') AS has_chronic_neurological_disease,
-        ("PNEUMOPATI" = '1') AS has_other_chronic_pneumopathy,
-        ("IMUNODEPRE" = '1') AS has_immunodeficiency,
-        ("RENAL" = '1') AS has_chronic_kidney_disease,
-        ("OBESIDADE" = '1') AS has_obesity
+        is_puerpera,
+        has_chronic_cardiovascular_disease, 
+        has_chronic_hematologic_disease, 
+        has_down_syndrome,
+        has_chronic_liver_disease,
+        has_asthma,
+        has_diabetes,
+        has_chronic_neurological_disease,
+        has_other_chronic_pneumopathy,
+        has_immunodeficiency,
+        has_chronic_kidney_disease,
+        has_obesity
     FROM
-        {{ source('opendatasus', 'opendatasus_srag_cases') }}
+        {{ ref('silver_srag_cases') }}
 )
 SELECT
     {{ dbt_utils.generate_surrogate_key([
--- src/transform/transformation.py
+++ src/transform/transformation.py
@@ -1,17 +1,17 @@
 import pandas as pd
-def correct_numbers(valor):
+def correct_numbers(value):
     """
     Corrige uma string de número onde tanto os separadores inteiros quanto o decimal são pontos.
     Ex: '1.234.56' -> '1234.56'
     """
-    if isinstance(valor, str):
+    if isinstance(value, str):
         # Conta quantos pontos existem na string
-        if valor.count('.') > 1:
+        if value.count('.') > 1:
             # Remove o primeiro ponto
             # O valor.count('.')-1 no final do replace diz para ele deixar apenas a última ocorrência do ponto
-            return valor.replace('.', '', valor.count('.') - 1)
+            return value.replace('.', '', value.count('.') - 1)
     # Se não for uma string ou se tiver 1 ou 0 pontos, retorna o valor como está
-    return valor
+    return value
 
 def transform_air_measurements(df_raw: pd.DataFrame) -> pd.DataFrame:
     """Aplica as transformações leves específicas para os dados do MonitorAr."""
@@ -24,7 +24,7 @@ def transform_air_measurements(df_raw: pd.DataFrame) -> pd.DataFrame:
 
 def transform_srag(df_raw: pd.DataFrame) -> pd.DataFrame:
     df = df_raw.copy()
-    colunas_para_converter_para_int = [
+    cols_to_convert = [
         'CS_SEXO', 'CS_RACA', 'CS_ESCOL_N', 'CS_GESTANT', 'FATOR_RISC',
         'PUERPERA', 'CARDIOPATI', 'HEMATOLOGI', 'SIND_DOWN', 'HEPATICA',
         'ASMA', 'DIABETES', 'NEUROLOGIC', 'PNEUMOPATI', 'IMUNODEPRE',
@@ -33,7 +33,7 @@ def transform_srag(df_raw: pd.DataFrame) -> pd.DataFrame:
         'FADIGA', 'PERD_OLFT', 'PERD_PALA', 'HOSPITAL', 'UTI',
         'SUPORT_VEN', 'CLASSI_FIN', 'CRITERIO', 'EVOLUCAO'
     ]
-    for col in colunas_para_converter_para_int:
+    for col in cols_to_convert:
         # Verifica se a coluna existe no DataFrame antes de tentar converter
         if col in df.columns:
             df[col] = pd.to_numeric(df[col], errors='coerce').astype('Int64')
